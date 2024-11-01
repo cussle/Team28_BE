b4f1aaaea41fa63622fe6d392f190a01cdca1d0b
donghyeon <thornappl2@gmail.com>
2024-11-01 15:35:22 +0900

    feat: add update Page

--- src/main/java/com/devcard/devcard/card/controller/page/CardPageController.java
+++ src/main/java/com/devcard/devcard/card/controller/page/CardPageController.java
@@ -2,8 +2,7 @@ package com.devcard.devcard.card.controller.page;
 
 import com.devcard.devcard.card.dto.CardResponseDto;
 import com.devcard.devcard.card.service.CardService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +13,8 @@ public class CardPageController {
 
     private final CardService cardService;
 
-    @Autowired
-    private Environment env;
+    @Value("${kakao.javascript.key}")
+    private String kakaoJavascriptKey;
 
     public CardPageController(CardService cardService) {
         this.cardService = cardService;
@@ -25,7 +24,14 @@ public class CardPageController {
     public String viewCard(@PathVariable Long id, Model model) {
         CardResponseDto card = cardService.getCard(id);
         model.addAttribute("card", card);
-        String kakaoJavascriptKey = env.getProperty("kakao.javascript.key");
+        model.addAttribute("kakaoJavascriptKey", kakaoJavascriptKey);
         return "cardDetail";
+    }
+
+    @GetMapping("/cards/{id}/update")
+    public String updateCard(@PathVariable Long id, Model model) {
+        CardResponseDto card = cardService.getCard(id);
+        model.addAttribute("card", card);
+        return "updateCard";
     }
 }

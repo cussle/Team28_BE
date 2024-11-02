07a8ce9e83442097d0488431fc4677a8721e5cc4
pswaao88 <66404498+pswaao88@users.noreply.github.com>
2024-11-02 22:25:06 +0900

    refactor: add update LastMessage logic

--- src/main/java/com/devcard/devcard/chat/model/ChatRoom.java
+++ src/main/java/com/devcard/devcard/chat/model/ChatRoom.java
@@ -21,7 +21,7 @@ public class ChatRoom {
     @JoinTable(name = "chat_room_participants")
     private List<ChatUser> participants;
     private LocalDateTime createdAt;
-    private String lastMessage;
+    private String lastMessage = "메세지를 보내보세요."; // 기본값 설정
     private LocalDateTime lastMessageTime;
 
     public ChatRoom(List<ChatUser> participants, LocalDateTime createdAt) {
@@ -30,7 +30,6 @@ public class ChatRoom {
     }
 
     protected ChatRoom() {
-
     }
 
     public List<ChatUser> getParticipants() {
@@ -56,5 +55,10 @@ public class ChatRoom {
     // participants로부터 name을 가져와 리스트화
     public List<String> getParticipantsName() {
         return this.participants.stream().map(ChatUser::getName).toList();
+    }
+    // lastMessage와 LastMessageTime 업데이트
+    public void updateLastMessageAndLastMessageTime(String newLastMessage, LocalDateTime newLastMessageTime) {
+        this.lastMessage = newLastMessage;
+        this.lastMessageTime = newLastMessageTime;
     }
 }

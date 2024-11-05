2d0718f994408b8c6725dbe59a97e47ed6f3cdd9
cussle <cussle@kakao.com>
2024-11-05 21:22:34 +0900

    chore: Seperate controllers into page for views, rest for APIs

--- src/main/java/com/devcard/devcard/chat/controller/page/ChatController.java
+++ src/main/java/com/devcard/devcard/chat/controller/page/ChatController.java
@@ -1,4 +1,4 @@
-package com.devcard.devcard.chat.controller;
+package com.devcard.devcard.chat.controller.page;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;

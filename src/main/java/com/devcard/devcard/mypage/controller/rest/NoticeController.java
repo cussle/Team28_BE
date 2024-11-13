5cfb9ed87f39534324138a45a0fa2ae2e4da3507
pswaao88 <66404498+pswaao88@users.noreply.github.com>
2024-11-14 04:36:06 +0900

    refactor: add path

--- src/main/java/com/devcard/devcard/mypage/controller/rest/NoticeController.java
+++ src/main/java/com/devcard/devcard/mypage/controller/rest/NoticeController.java
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
-@RequestMapping()
+@RestController
+@RequestMapping("/notice")
 public class NoticeController {
 
     private final NoticeService noticeService;

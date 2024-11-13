a2ac928ae3b8d2cc39f2c0da11e5cb49fe7f1a3a
HaegyeongKim01 <haegyeong.kim01@gmail.com>
2024-11-14 00:42:54 +0900

    feat: Qrcode 생성  UI 구현

--- src/main/java/com/devcard/devcard/auth/config/SecurityConfig.java
+++ src/main/java/com/devcard/devcard/auth/config/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
                             "/js/**",
                             "/images/**",
                             "/h2-console/**",
-                            "/walletList"
+                            "/walletList",
+                            "/qrcodes/**"
                     ).permitAll();
                     // 그 외의 모든 요청은 인증 필요
                     auth.anyRequest().authenticated();

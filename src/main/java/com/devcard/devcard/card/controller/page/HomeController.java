6fdf2f81eacb23353e54521d1f67f20cf2ed49b6
donghyeon <thornappl2@gmail.com>
2024-11-14 18:10:28 +0900

    feat: 로그인 시 계정 정보 연동

--- src/main/java/com/devcard/devcard/card/controller/page/HomeController.java
+++ src/main/java/com/devcard/devcard/card/controller/page/HomeController.java
@@ -1,6 +1,10 @@
 package com.devcard.devcard.card.controller.page;
 
+import com.devcard.devcard.auth.entity.Member;
+import com.devcard.devcard.auth.model.OauthMemberDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -8,7 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/home")
-    public String home() {
+    public String home(@AuthenticationPrincipal OauthMemberDetails oauthMemberDetails, Model model) {
+        if (oauthMemberDetails != null) {
+            Member member = oauthMemberDetails.getMember();
+            model.addAttribute("member", member);
+        }
         return "home";
     }
 }

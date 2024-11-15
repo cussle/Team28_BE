61a0ccbb3e4b8f747d394bef846909934f04ac22
cussle <cussle@kakao.com>
2024-11-15 15:57:25 +0900

    fix: Add default constructor and accessors to CreateRoomRequest DTO

--- src/main/java/com/devcard/devcard/chat/dto/CreateRoomRequest.java
+++ src/main/java/com/devcard/devcard/chat/dto/CreateRoomRequest.java
@@ -6,10 +6,20 @@ public class CreateRoomRequest {
 
     private List<Long> participantsId;
 
-    public <T> CreateRoomRequest(List<T> list) {
+    // 기본 생성자
+    public CreateRoomRequest() {
+    }
+
+    // 모든 필드 생성자
+    public CreateRoomRequest(List<Long> participantsId) {
+        this.participantsId = participantsId;
     }
 
     public List<Long> getParticipantsId() {
         return participantsId;
+    }
+
+    public void setParticipantsId(List<Long> participantsId) {
+        this.participantsId = participantsId;
     }
 }

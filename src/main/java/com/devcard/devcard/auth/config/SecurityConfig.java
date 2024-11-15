b7cb785ab50e4aea0399be6133deaf46a44b63e7
cussle <cussle@kakao.com>
2024-11-15 12:59:37 +0900

    feat: Set initial lastMessageTime to prioritize new chat rooms

--- src/main/java/com/devcard/devcard/auth/config/SecurityConfig.java
+++ src/main/java/com/devcard/devcard/auth/config/SecurityConfig.java
@@ -38,7 +38,8 @@ public class SecurityConfig {
                             "/notice",
                             "/notice/**",
                             "/qna",
-                            "/qna/**"
+                            "/qna/**",
+                            "/api/**"  // 테스트용 임시
                     ).permitAll();
                     // 그 외의 모든 요청은 인증 필요
                     auth.anyRequest().authenticated();
